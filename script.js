3e887ede3df57c23798181bb54d1a710593e7e40
monstro1 <michael@monstro1.com>
2023-10-02 13:33:27 -0400

    Finish game function

--- script.js
+++ script.js
@@ -11,17 +11,15 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
-	playerSelection = playerSelection.toLowerCase();
-	playerSelectionCapitalized = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
 	if (playerSelection !== "rock" && playerSelection !== "paper" && playerSelection !== "scissors") {
-		return "Error: " + playerSelection + " is not a valid choice!";
+		return "error";
 	}
 	if (playerSelection === computerSelection) {
-		return "It's a tie! Both players selected " + playerSelection;
+		return "draw";
 	} else if (playerSelection === "rock" && computerSelection === "scissors" || playerSelection === "paper" && computerSelection === "rock" || playerSelection === "scissors" && computerSelection === "paper") {
-		return "You win! " + playerSelectionCapitalized + " beats " + computerSelection;
+		return "win";
 	} else {
-		return "You lose! " + playerSelectionCapitalized + " loses to " + computerSelection;
+		return "loss";
 	}
 }
 
@@ -29,7 +27,30 @@ function game() {
 	let playerScore = 0;
 	let computerScore = 0;
 	for (let i = 0; i < 5; i++) {
-		let playerChoice = prompt("Choose rock, paper, or scissors");
-		playRound(playerChoice, getComputerChoice());
+		let playerChoice = prompt("Choose rock, paper, or scissors").toLowerCase();
+		let playerChoiceCapitalized = playerChoice.charAt(0).toUpperCase() + playerChoice.slice(1);
+		let computerChoice = getComputerChoice();
+		let gameResult = playRound(playerChoice, computerChoice);
+		switch (gameResult) {
+			case "error":
+				return `Error: ${playerChoice} is not a valid choice.`;
+			case "draw":
+				console.log(`It's a tie! Both players selected ${playerChoice}.`);
+				break;
+			case "win":
+				playerScore++;
+				console.log(`You win! ${playerChoiceCapitalized} beats ${computerChoice}.`);
+				break;
+			default:
+				computerScore++;
+				console.log(`You lose! ${playerChoiceCapitalized} loses to ${computerChoice}.`);
+		}
+	}
+	if (playerScore === computerScore) {
+		return `It's a tie! Both players have a score of ${playerScore}.`;
+	} else if (playerScore > computerScore) {
+		return `You win! Your score: ${playerScore}; the computer's score: ${computerScore}.`;
+	} else {
+		return `You lose! The computer's score: ${computerScore}; your score: ${playerScore}.`;
 	}
 }
