d2c8de4903b584580134619bea08558cc838f745
monstro1 <michael@monstro1.com>
2023-10-02 12:59:34 -0400

    Add playRound function

--- script.js
+++ script.js
@@ -9,3 +9,18 @@ function getComputerChoice() {
 			return "scissors";
 	}
 }
+
+function playRound(playerSelection, computerSelection) {
+	playerSelection = playerSelection.toLowerCase();
+	playerSelectionCapitalized = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
+	if (playerSelection !== "rock" && playerSelection !== "paper" && playerSelection !== "scissors") {
+		return "Error: " + playerSelection + " is not a valid choice!";
+	}
+	if (playerSelection === computerSelection) {
+		return "It's a tie! Both players selected " + playerSelection;
+	} else if (playerSelection === "rock" && computerSelection === "scissors" || playerSelection === "paper" && computerSelection === "rock" || playerSelection === "scissors" && computerSelection === "paper") {
+		return "You win! " + playerSelectionCapitalized + " beats " + computerSelection;
+	} else {
+		return "You lose! " + playerSelectionCapitalized + " loses to " + computerSelection;
+	}
+}
